5ba249af93c5070abb9cfca90228b9a4282366e2
YusofAAA <76404657+YusofAAA@users.noreply.github.com>
2021-02-11 14:42:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import string
+def random_string(length=32):
+    character_set = string.ascii_letters
+    return ''.join(random.choice(character_set) for i in range(length))
+
+for i in range(int(input('How many codes? '))):
+    codes = random_string(24)
+    print('Code: ' + 'https://discord.gift/ + codes)
+input()
